bc1b7181c8db9e53ad3ac758c7ae542111f3b08b
canmom <bryn.dickinson@gmail.com>
2019-09-14 00:15:30 +0100

    Improve HTML-building bash script

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-kramdown VECTOR.md | cat vector-preamble.html - vector-footer.html > VECTOR-ch1.html;
-kramdown VECTOR-ch2.md | cat vector-preamble.html - vector-footer.html > VECTOR-ch2.html;
-kramdown VECTOR-ch3.md | cat vector-preamble.html - vector-footer.html > VECTOR-ch3.html;
-kramdown VECTOR-ch4.md | cat vector-preamble.html - vector-footer.html > VECTOR-ch4.html
+#!/bin/bash
+prefix="VECTOR-ch"
+
+for i in `seq 1 5`; do
+  kramdown "$prefix$i.md" | cat vector-preamble.html - vector-footer.html > "$prefix$i.html"; 
+done
