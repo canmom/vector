e2ba8082e4f4979c99f2b9caab8bbc96d4545618
canmom <bryn.dickinson@gmail.com>
2019-11-18 00:19:26 +0000

    Tweak build script for web upload

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 prefix="VECTOR-ch"
 
-for i in `seq 1 5`; do
-  kramdown "$prefix$i.md" | cat vector-preamble.html - vector-footer.html > "$prefix$i.html"; 
+for i in `seq 1 6`; do
+  kramdown "$prefix$i.md" | cat vector-preamble.html - vector-footer.html > "ch$i.html"; 
 done
