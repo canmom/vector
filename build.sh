5047c4e0ce01c1929093d5667e9a66429a3519cd
canmom <bryn.dickinson@gmail.com>
2019-12-03 16:25:50 +0000

    Add navigation links on chapter pages

--- build.sh
+++ build.sh
@@ -1,6 +1,30 @@
 #!/bin/bash
 prefix="VECTOR-ch"
+navigation=$(<navigation.html)
+preamble=$(<vector-preamble.html)
+footer=$(<vector-footer.html)
+chapterTitles=(null FIRST SECOND THIRD FOURTH FIFTH SIXTH SEVENTH EIGHT NINTH TENTH ELEVENTH TWELFTH)
 
 for i in `seq 1 6`; do
-  kramdown "$prefix$i.md" | cat vector-preamble.html - vector-footer.html > "ch$i.html"; 
+    prev=$((i-1));
+    next=$((i+1));
+    thisNavigation=$navigation
+    if ((prev>0))
+    then
+        thisNavigation=${thisNavigation/"{{prev}}"/$prev};
+        thisNavigation=${thisNavigation/"{{prevname}}"/${chapterTitles[prev]}};
+        thisNavigation=${thisNavigation/"{{notfirst}}"/};
+    else
+        thisNavigation=$(sed 's#{{notfirst}}.*</a>##' <<< $thisNavigation);
+    fi
+    if ((next<13))
+    then
+        thisNavigation=${thisNavigation/"{{next}}"/$next};
+        thisNavigation=${thisNavigation/"{{nextname}}"/${chapterTitles[next]}};
+        thisNavigation=${thisNavigation/"{{notlast}}"/};
+    else
+        thisNavigation=$(sed 's#{{notlast}}.*</a>##' <<< $thisNavigation);
+    fi
+    echo "$(<vector-preamble.html)$(kramdown "$prefix$i.md")$thisNavigation$(<vector-footer.html)" > "ch$i.html";
+    echo "Processed chapter $i";
 done
